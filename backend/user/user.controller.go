5df1eb548761a1b65e383944002f0fcee8c7f0f8
Airat Khisamiev <aira3t@gmail.com>
2024-01-17 00:50:58 +0700

    Update api docs, update user setting dto

--- backend/user/user.controller.go
+++ backend/user/user.controller.go
@@ -59,7 +59,7 @@ func handlePutUser(app *fiber.App) {
 // @Description  Get user info
 // @Tags         user
 // @Param        Authorization header string true "Bearer"
-// @Param        userId   path      string  true  ":userId|me"
+// @Param        userId   path      string  true  "uuid or me - to get info for the logged in user"
 // @Produce      json
 // @Success      200  {object} user.UserResult
 // @Failure      400  {object} helpers.ErrorResponse
