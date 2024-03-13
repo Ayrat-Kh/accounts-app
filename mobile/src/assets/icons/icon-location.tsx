ba36a3843a7a62b665bfd25716660016ed20a7d3
Airat Khisamiev <aira3t@gmmail.com>
2024-03-13 23:20:44 +0100

    Add location and scroll icons

---
+++ mobile/src/assets/icons/icon-location.tsx
@@ -0,0 +1,25 @@
+import { Path, Svg } from 'react-native-svg';
+
+import { useStrokeColor } from '~/ui/hooks';
+
+import type { BaseSettingProps } from './types';
+
+export const IconLocation: React.FC<BaseSettingProps> = ({
+  size = 50,
+  color = 'primary',
+  ...rest
+}: BaseSettingProps) => {
+  const strokeColor = useStrokeColor(color);
+
+  return (
+    <Svg width={size} height={size} viewBox="0 0 24 24" {...rest}>
+      <Path
+        stroke={strokeColor}
+        d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
+        strokeLinecap="round"
+        strokeWidth={2}
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+};
--- mobile/src/assets/icons/index.ts
+++ mobile/src/assets/icons/index.ts
@@ -3,3 +3,5 @@ export * from './icon-close-outlined-no-border';
 export * from './icon-google';
 export * from './icon-setting';
 export * from './icon-book';
+export * from './icon-scroll-v';
+export * from './icon-location';
