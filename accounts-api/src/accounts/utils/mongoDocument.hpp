39f4580cb316a591c6171b2ac2f4c3b0e228f9c2
Airat Khisamiev <aira3t@gmmail.com>
2024-04-11 23:29:16 +0200

    Unlock parser for gcc

--- accounts-api/src/accounts/utils/mongoDocument.hpp
+++ accounts-api/src/accounts/utils/mongoDocument.hpp
@@ -50,160 +50,6 @@ namespace accounts::utils
         using type = TArg;
     };
 
-    template <class T>
-    static T deserializeMongoDocument(document::view view);
-
-    // template <class TElement = document::view>
-    // struct MongoSerializeHelpers
-    // {
-    //     template <class T>
-    //     static T deserializeScalar(const TElement &element);
-
-    //     template <>
-    //     std::optional<std::string> deserializeScalar(const TElement &element)
-    //     {
-    //         return element.type() == bsoncxx::type::k_string ? std::move(std::string(element.get_string().value)) : std::optional<std::string>{};
-    //     }
-    //     template <>
-    //     std::string deserializeScalar(const TElement &element)
-    //     {
-    //         auto res = std::move(MongoSerializeHelpers<TElement>::deserializeScalar<std::optional<std::string>>(element));
-    //         return res.has_value() ? std::move(res.value()) : 0;
-    //     }
-
-    //     template <>
-    //     std::optional<double> deserializeScalar(const TElement &element)
-    //     {
-    //         return element.type() == bsoncxx::type::k_double ? element.get_double().value : std::optional<double>{};
-    //     }
-    //     template <>
-    //     double deserializeScalar(const TElement &element)
-    //     {
-    //         auto res = std::move(MongoSerializeHelpers<TElement>::deserializeScalar<std::optional<double>>(element));
-    //         return res.has_value() ? res.value() : 0.f;
-    //     }
-
-    //     template <>
-    //     std::optional<std::int64_t> deserializeScalar(const TElement &element)
-    //     {
-    //         return element.type() == bsoncxx::type::k_int64 ? element.get_int64().value : std::optional<std::int64_t>{};
-    //     }
-    //     template <>
-    //     std::int64_t deserializeScalar(const TElement &element)
-    //     {
-    //         auto res = std::move(MongoSerializeHelpers<TElement>::deserializeScalar<std::optional<std::int64_t>>(element));
-    //         return res.has_value() ? res.value() : 0.f;
-    //     }
-
-    //     template <>
-    //     std::optional<std::int32_t> deserializeScalar(const TElement &element)
-    //     {
-    //         return element.type() == bsoncxx::type::k_int32 ? element.get_int32().value : std::optional<std::int32_t>{};
-    //     }
-    //     template <>
-    //     std::int32_t deserializeScalar(const TElement &element)
-    //     {
-    //         auto res = std::move(MongoSerializeHelpers<TElement>::deserializeScalar<std::optional<std::int32_t>>(element));
-    //         return res.has_value() ? res.value() : 0.f;
-    //     }
-
-    //     template <>
-    //     std::optional<::accounts::shared::Datetime> deserializeScalar(const TElement &element)
-    //     {
-    //         return element.type() == bsoncxx::type::k_date
-    //                    ? std::chrono::system_clock::from_time_t(0) + std::chrono::milliseconds(element.get_date().to_int64())
-    //                    : std::optional<::accounts::shared::Datetime>{};
-    //     }
-    //     template <>
-    //     ::accounts::shared::Datetime deserializeScalar(const TElement &element)
-    //     {
-    //         auto res = std::move(MongoSerializeHelpers<TElement>::deserializeScalar<std::optional<::accounts::shared::Datetime>>(element));
-    //         return res.has_value() ? res.value() : ::accounts::shared::Datetime{};
-    //     }
-
-    //     template <class T>
-    //     static T deserializeVector(const TElement &element)
-    //     {
-    //         T result = {};
-
-    //         if (element.type() != bsoncxx::type::k_array)
-    //         {
-    //             return std::move(result);
-    //         }
-
-    //         for (auto &elem : element.get_array().value)
-    //         {
-    //             result.push_back(std::move(MongoSerializeHelpers<bsoncxx::array::element>::handleTypeDeserialization<typename T::value_type>(elem)));
-    //         }
-
-    //         return std::move(result);
-    //     }
-
-    //     template <class T>
-    //     static T deserializeObject(const TElement &element)
-    //     {
-    //         if (element.type() == bsoncxx::type::k_document)
-    //         {
-    //             return std::move(deserializeMongoDocument<T>(element.get_document().value));
-    //         }
-    //         return std::move(T{});
-    //     }
-
-    //     template <class TReturn>
-    //     static constexpr TReturn handleTypeDeserialization(const TElement &element)
-    //     {
-    //         if constexpr (
-    //             std::is_same<
-    //                 TReturn, std::string>::value ||
-    //             std::is_same<
-    //                 TReturn, std::optional<std::string>>::value ||
-    //             std::is_same<TReturn, std::int32_t>::value ||
-    //             std::is_same<TReturn, std::optional<std::int32_t>>::value ||
-    //             std::is_same<TReturn, std::int64_t>::value ||
-    //             std::is_same<TReturn, std::optional<std::int64_t>>::value ||
-    //             std::is_same<TReturn, double>::value ||
-    //             std::is_same<TReturn, std::optional<double>>::value ||
-    //             std::is_same<TReturn, ::accounts::shared::Datetime>::value ||
-    //             std::is_same<TReturn, std::optional<::accounts::shared::Datetime>>::value)
-    //         {
-    //             return std::move(MongoSerializeHelpers<TElement>::deserializeScalar<TReturn>(element));
-    //         }
-    //         else if constexpr (is_vector<TReturn>::value)
-    //         {
-    //             return std::move(MongoSerializeHelpers<TElement>::deserializeVector<TReturn>(element));
-    //         }
-    //         else if constexpr (std::is_class<TReturn>::value)
-    //         {
-    //             return std::move(MongoSerializeHelpers<TElement>::deserializeObject<TReturn>(element));
-    //         }
-
-    //         assert((void("Uncovered type"), 0));
-    //     }
-    // };
-
-    template <class T>
-    static T deserializeMongoDocument(document::view view)
-    {
-        using D1 = boost::describe::describe_members<T, boost::describe::mod_public>;
-        T result;
-
-        // boost::mp11::mp_for_each<D1>(
-        //     [&view, &result](auto D)
-        //     {
-        //         using return_t = std::remove_reference<boost::callable_traits::return_type_t<decltype(D.pointer)>>::type;
-
-        //         document::element element = view[D.name == "id" ? "_id" : D.name];
-
-        //         result.*D.pointer = std::move(MongoSerializeHelpers<document::element>::handleTypeDeserialization<return_t>(element));
-        //     });
-
-        return std::move(result);
-    }
-
-    /** Below is to mongo implementation */
-    template <class T>
-    bsoncxx::builder::basic::document toMongoDocument(T object);
-
     template <class TItem>
     struct is_primitive
     {
@@ -215,6 +61,183 @@ namespace accounts::utils
             std::is_same<TItem, double>::value ||
             std::is_same<TItem, ::accounts::shared::Datetime>::value;
     };
+
+    template <class T>
+    static T deserializeMongoDocument(document::view view);
+
+    template <class TElement, class T>
+    struct MongoScalarSerialize
+    {
+        static T deserializeScalar(const TElement &element)
+        {
+            return T{};
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::string>
+    {
+        static std::string deserializeScalar(const TElement &element)
+        {
+            auto res = std::move(MongoScalarSerialize<TElement, std::optional<std::string>>::deserializeScalar(element));
+            return res.has_value() ? std::move(res.value()) : 0;
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::optional<std::string>>
+    {
+        static std::optional<std::string> deserializeScalar(const TElement &element)
+        {
+            return element.type() == bsoncxx::type::k_string ? std::move(std::string(element.get_string().value)) : std::optional<std::string>{};
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, double>
+    {
+        static double deserializeScalar(const TElement &element)
+        {
+            auto res = std::move(MongoScalarSerialize<TElement, std::optional<double>>::deserializeScalar(element));
+            return res.has_value() ? std::move(res.value()) : 0;
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::optional<double>>
+    {
+        static std::optional<double> deserializeScalar(const TElement &element)
+        {
+            return element.type() == bsoncxx::type::k_double ? element.get_double().value : std::optional<double>{};
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::int64_t>
+    {
+        static std::int64_t deserializeScalar(const TElement &element)
+        {
+            auto res = std::move(MongoScalarSerialize<TElement, std::optional<std::int64_t>>::deserializeScalar(element));
+            return res.has_value() ? std::move(res.value()) : 0;
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::optional<std::int64_t>>
+    {
+        static std::optional<std::int64_t> deserializeScalar(const TElement &element)
+        {
+            return element.type() == bsoncxx::type::k_int64 ? element.get_int64().value : std::optional<std::int64_t>{};
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::int32_t>
+    {
+        static std::int32_t deserializeScalar(const TElement &element)
+        {
+            auto res = std::move(MongoScalarSerialize<TElement, std::optional<std::int32_t>>::deserializeScalar(element));
+            return res.has_value() ? std::move(res.value()) : 0;
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::optional<std::int32_t>>
+    {
+        static std::optional<std::int32_t> deserializeScalar(const TElement &element)
+        {
+            return element.type() == bsoncxx::type::k_int32 ? element.get_int32().value : std::optional<std::int32_t>{};
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, ::accounts::shared::Datetime>
+    {
+        static ::accounts::shared::Datetime deserializeScalar(const TElement &element)
+        {
+            auto res = std::move(MongoScalarSerialize<TElement, std::optional<::accounts::shared::Datetime>>::deserializeScalar(element));
+            return res.has_value() ? std::move(res.value()) : ::accounts::shared::Datetime{};
+        }
+    };
+    template <class TElement>
+    struct MongoScalarSerialize<TElement, std::optional<::accounts::shared::Datetime>>
+    {
+        static std::optional<::accounts::shared::Datetime> deserializeScalar(const TElement &element)
+        {
+            return std::optional<::accounts::shared::Datetime>{};
+            return element.type() == bsoncxx::type::k_date
+                       ? std::chrono::system_clock::from_time_t(0) + std::chrono::milliseconds(element.get_date().to_int64())
+                       : std::optional<::accounts::shared::Datetime>{};
+        }
+    };
+
+    template <class TElement>
+    struct MongoSerializeHelpers
+    {
+        template <class T>
+        static T deserializeVector(const TElement &element)
+        {
+            T result = {};
+
+            if (element.type() != bsoncxx::type::k_array)
+            {
+                return std::move(result);
+            }
+
+            for (auto &elem : element.get_array().value)
+            {
+                result.push_back(std::move(MongoSerializeHelpers<bsoncxx::array::element>::handleTypeDeserialization<typename T::value_type>(elem)));
+            }
+
+            return std::move(result);
+        }
+
+        template <class T>
+        static T deserializeObject(const TElement &element)
+        {
+            if (element.type() == bsoncxx::type::k_document)
+            {
+                return std::move(deserializeMongoDocument<T>(element.get_document().value));
+            }
+            return std::move(T{});
+        }
+
+        template <class TReturn>
+        static constexpr TReturn handleTypeDeserialization(const TElement &element)
+        {
+            using real_t = typename unwrap_optional<TReturn>::type;
+
+            if constexpr (is_primitive<real_t>::value)
+
+            {
+                return std::move(MongoScalarSerialize<TElement, TReturn>::deserializeScalar(element));
+            }
+            else if constexpr (is_vector<TReturn>::value)
+            {
+                return std::move(MongoSerializeHelpers<TElement>::deserializeVector<TReturn>(element));
+            }
+            else if constexpr (std::is_class<TReturn>::value)
+            {
+                return std::move(MongoSerializeHelpers<TElement>::deserializeObject<TReturn>(element));
+            }
+
+            assert((void("Uncovered type"), 0));
+        }
+    };
+
+    template <class T>
+    static T deserializeMongoDocument(document::view view)
+    {
+        using D1 = boost::describe::describe_members<T, boost::describe::mod_public>;
+        T result;
+
+        boost::mp11::mp_for_each<D1>(
+            [&view, &result](auto D)
+            {
+                using return_t = std::remove_reference<boost::callable_traits::return_type_t<decltype(D.pointer)>>::type;
+
+                document::element element = view[D.name == "id" ? "_id" : D.name];
+
+                result.*D.pointer = std::move(MongoSerializeHelpers<document::element>::handleTypeDeserialization<return_t>(element));
+            });
+
+        return std::move(result);
+    }
+
+    /** Below is to mongo implementation */
+    template <class T>
+    bsoncxx::builder::basic::document toMongoDocument(T object);
 
     template <class T>
     inline void serializeScalar(bsoncxx::builder::basic::document &document, const std::string &name, T value);
@@ -303,8 +326,6 @@ namespace accounts::utils
             serializeObject(document, name, std::move(item));
             return;
         }
-
-        // assert((void("Uncovered type"), 0));
     }
 
     template <class T>
