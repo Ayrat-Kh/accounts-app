0601a8b2aa71a3d7b818663300be6d96b7911e51
Airat Khisamiev <aira3t@gmmail.com>
2024-05-01 23:10:57 +0200

    Rename build scripts

--- accounts-api/run-app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ACCOUNTS_DB_URL=mongodb://localhost:27017
-export ACCOUNTS_JWT_KEY=superDuperKey
-export ACCOUNTS_PORT=3000
-export ACCOUNTS_DB=accounts
-export ACCOUNTS_GEOCODE_API_KEY=662e22a87cafd117668634rcv1c0b93
-
-./build/AccountsApi
